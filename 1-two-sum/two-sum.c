1d4629a7d575ca5538f277b05a62c8b0fc4ea9dd
Mohamed ElBerry <mohamed_berry210@hotmail.com>
2025-09-06 17:38:21 +0300

    Time: 227 ms (5.05%) | Memory: 8.6 MB (92.68%) - LeetSync

--- 1-two-sum/two-sum.c
+++ 1-two-sum/two-sum.c
@@ -4,7 +4,7 @@
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     for(int i = 0 ; i< numsSize; i++ )
     {
-        for(int j = 0 ; j< numsSize ; j++ )
+        for(int j = 1 ; j< numsSize ; j++ )
         {
             if ((i != j) && (nums[i] + nums[j] == target))
             {
