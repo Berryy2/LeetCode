e8299427b01ddecad69529dbc739ded6aabe9ab8
Mohamed ElBerry <mohamed_berry210@hotmail.com>
2025-09-06 18:45:47 +0300

    Time: 3 ms (93.83%) | Memory: 10.2 MB (5.07%) - LeetSync

--- 1-two-sum/two-sum.c
+++ 1-two-sum/two-sum.c
@@ -1,7 +1,8 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+ // First Answer -> the first Idea will come to new members in leetcode.
+/*int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     for(int i = 0 ; i< numsSize; i++ )
     {
         for(int j = 1 ; j< numsSize ; j++ )
@@ -17,5 +18,52 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
         }
     }
     *returnSize = 0;
+    return NULL;
+}*/
+//solving for lineat time complexity using hashTable approach.
+//#include "uthash.h" //Library used to take the HashTable approach in C
+// making our hash table 
+typedef struct{
+    int index; // For index which we will return 
+    int key;  // For the index's value
+    UT_hash_handle hh; //the name of the handle in the struct
+}ht;
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+ht *table = NULL; // Pointer points to the head of the hashtabel 
+ht *entry = NULL; // pointer to be used for Operations like Hash Add/Find
+for(int i = 0; i < numsSize; i++)
+{
+int compliment = target - nums[i]; // Store the comp. value so, if we found 
+                                // it in the table we then got our value
+HASH_FIND_INT(table, &compliment, entry);
+if(entry) //if we found the compliment in the table.
+{
+    int *res = malloc(2* sizeof(int));
+    res[0] = entry -> index;
+    res[1] = i;
+    *returnSize = 2; //needed from the problem.
+    //Then we free our table to clear the memory.
+    ht *current, *temp; //If we delete cur during iteration, you’d lose track 
+                        //of the next element. HASH_ITER stores it in tmp so 
+                        //iteration is safE.
+    HASH_ITER(hh, table, current, temp)
+    {
+        HASH_DEL(table, current);
+        free(current);
+    } //You can also use HASH_CLEAR directly
+    return res; 
+}
+    entry = malloc(sizeof(ht));
+    entry -> key = nums[i]; // Storing value in the i's index,
+    entry -> index = i;    // Storing the index itself.
+    HASH_ADD_INT(table, key, entry);
+    }
+    *returnSize = 0;
+    ht *current, *temp;
+    HASH_ITER(hh, table, current, temp)
+    {
+        HASH_DEL(table, current);
+        free(current);
+    }
     return NULL;
 }
