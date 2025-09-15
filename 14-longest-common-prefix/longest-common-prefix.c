bfa3dc884d25d654902aae981c1b4a8a451b4faf
Mohamed ElBerry <mohamed_berry210@hotmail.com>
2025-09-15 17:50:11 +0300

    Time: 3 ms (6.12%) | Memory: 8.2 MB (58.79%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.c
@@ -0,0 +1,14 @@
+char* longestCommonPrefix(char** strs, int strsSize) 
+{
+
+for(int i = 0; i < strlen(strs[0]); i++)
+{
+    char c = strs[0][i];
+    for(int j = 1; j < strsSize; j++)
+    {
+        if((i == strlen(strs[j])) || (strs[j][i] != c))
+        return strndup(strs[0], i); 
+    }
+}
+return strs[0];
+}
