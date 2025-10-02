ff00eaa70fef9aebf6ced096eb185bd75156762d
Mohamed ElBerry <mohamed_berry210@hotmail.com>
2025-10-02 16:28:05 +0300

    Time: 0 ms (100.00%) | Memory: 7.9 MB (22.79%) - LeetSync

--- 1642-water-bottles/water-bottles.c
+++ 1642-water-bottles/water-bottles.c
@@ -19,3 +19,20 @@ int numWaterBottles(int numBottles, int numExchange)
     // Return the maximum number of bottles that can be drunk.
     return total;
 }
+/*
+"Simply and shortly each time you exchange X empty bottles (-X) you get on full which
+you will drink to be empty -> (-X + 1) = -(X - 1).
+the key is to know how many time you can exchange (Y)
+for ex: you have 9 bottles and exchange  = 3.
+according to what we say the every trade cost you 2 bottles -(3 - 1)
+How many “2’s” can you subtract until you’re left with fewer than 3?
+9 → 7 → 5 → 3 → 1. That’s 4 exchanges.
+Formula: (9 – 1)/(3 – 1) = 8/2 = 4. Matches.
+and why 9 (- 1) because the last empty bottles will not be exchanged at the end 
+so it's useless to count it.""
+
+int numWaterBottles(int numBottles, int numExchange) {
+    // problem guarantees numExchange >= 2
+    return numBottles + (numBottles - 1) / (numExchange - 1);
+}
+*/
