2f894446c55e31deb732b82693f7c180b24e574c
Mohamed ElBerry <mohamed_berry210@hotmail.com>
2025-10-02 15:57:19 +0300

    Time: 0 ms (100.00%) | Memory: 7.7 MB (93.36%) - LeetSync

--- 1642-water-bottles/water-bottles.c
+++ 1642-water-bottles/water-bottles.c
@@ -1,11 +1,21 @@
+// Here you can see my O(log(n)) answer and there is an O(1) geometric form;
 int numWaterBottles(int numBottles, int numExchange) 
 {
+    // Total keeps track of how many bottles have been drunk.
     int total = numBottles;
+    // Continue as long as we have enough empty bottles to exchange.
     while (numBottles >= numExchange)
     {
-    int NotChangedBottles = numBottles / numExchange;
-    numBottles = NotChangedBottles + (numBottles % numExchange);
-    total += NotChangedBottles;
+    // How many new full bottles we get by exchanging.
+    // using that we by default take the floor of this division.
+    int ChangedBottles = numBottles / numExchange;
+    // Update the current bottle count:
+    //   - the newly obtained full bottles (changed)
+    //   - plus the leftover empties that weren’t enough to exchange.
+    numBottles = ChangedBottles + (numBottles % numExchange);
+    // Add the new full bottles to the total drunk count.
+    total += ChangedBottles;
     }
+    // Return the maximum number of bottles that can be drunk.
     return total;
 }
