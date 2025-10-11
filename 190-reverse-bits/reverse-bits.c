4b66b8700f4910ee985091cb15d1e3cd8258bc71
Mohamed ElBerry <mohamed_berry210@hotmail.com>
2025-10-11 13:46:38 +0300

    Time: 4 ms (30.43%) | Memory: 8.2 MB (25.27%) - LeetSync

--- 190-reverse-bits/reverse-bits.c
+++ 190-reverse-bits/reverse-bits.c
@@ -1,14 +1,36 @@
+/********************************************************************************************************
+* This is a bit manipulation problem where we reverse the bits of a 32-bit unsigned integer.
+* We iterate 32 times (once for each bit), shifting bits from 'n' into 'rev' in reverse order.
+* In each iteration, we:
+*   1. Shift 'rev' left to make space for the next bit.
+*   2. Copy the least significant bit of 'n' into 'rev' (if it’s 1).
+*   3. Shift 'n' right to process the next bit.  
+*********************************************************************************************************/
 int reverseBits(int n) 
 {
- int rev = 0;
- for(int i = 0; i < 32; i++)
- {
-    rev <<= 1;
-    if(n & 1)
+    // Initiate rev to zero.
+    int rev = 0;
+    // Loop through the number of bits of the give input.
+    for(int i = 0; i < 32; i++)
     {
+        // We leave space for the next bit to be inserted.
+        // ex: iteration 1
+        //     rev = 0000
+        //     n   = 1011
+        //     iteration 2
+        //     rev = 0001
+        //     n   = 0101
+        // then we leave space for rev making it: 0010 -> to insert the reversed bit without affecting
+        // previous ones.
+        rev <<= 1;
+        // If the bit in n = 1 we insert it in rev by OR operator.
+        if(n & 1)
+        {
         rev |= 1;
-    }
-    n >>= 1;
+        }
+        // We shift n to the right to see the next most siginificant bit and so on..
+        n >>= 1;
  }   
- return rev;
+    // At the end returned the reversed bit number.
+    return rev;
 }
