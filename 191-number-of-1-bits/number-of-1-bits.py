ea7ffef40500ef466c2d24836fa37ebeab9d1cb3
Mohamed ElBerry <mohamed_berry210@hotmail.com>
2025-10-13 17:38:24 +0300

    Time: 0 ms (100.00%) | Memory: 12.5 MB (9.52%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -4,9 +4,18 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        # Looping and increments counter each time an (n)'s bit is 1 checking by AND operation.
+        # then at the end of the loop shift (n) bits to the right. 
+
+        # Intialize counter by zero.
         counter = 0
+
+        # Looping in the 32 bits. 
         for i in range (32):
+            # If the bit is "1" increment counter. 
             if n & 1:
                 counter += 1
+            # Shift (n) to the right.
             n >>= 1
+        # At the end return counetr.
         return counter
