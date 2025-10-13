d4d0eee1a7da059a5547b7e9d8dc5d25f01912d1
Mohamed ElBerry <mohamed_berry210@hotmail.com>
2025-10-13 17:33:25 +0300

    Time: 2 ms (22.84%) | Memory: 12.3 MB (80.19%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        counter = 0
+        for i in range (32):
+            if n & 1:
+                counter += 1
+            n >>= 1
+        return counter
