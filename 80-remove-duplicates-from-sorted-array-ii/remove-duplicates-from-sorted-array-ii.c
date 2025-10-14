e7f6dcca3b13949c7c4b788ebb0dba4e345826a4
Mohamed ElBerry <mohamed_berry210@hotmail.com>
2025-10-14 20:55:08 +0300

    Time: 29 ms (42.30%) | Memory: 14.6 MB (19.03%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.c
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.c
@@ -1,24 +1,50 @@
+/********************************************************************************************************
+* In this problem, we are given a sorted array and must remove extra duplicates in-place
+* so that each unique element appears at most twice.
+* We will go with two pointers approach:
+*   - F (Front pointer): iterates through the array checking for duplicates.
+*   - R (Rear pointer): marks the position where the next valid element should be placed.
+*   - counter to track how many times the current element has appeared so far.
+*********************************************************************************************************/
 int removeDuplicates(int* nums, int numsSize) 
 {
+    // Corner case: If array size is less than or equal to 2, all elements are valid.
+    if (numsSize <= 2) return numsSize;
+
+    // Initialize pointers:
+    // Front pointer to iterate through the array.
     int F = 1;
+    // Rear pointer to mark next valid position.
     int R = 1;
+    // Counts occurrences of the current number.
     int counter = 1;
+
+    // Iterate through the array starting from the second element.
     while(F < numsSize)
     {
+        // If current number equals previous, increment counter.
         if(nums[F] == nums[F - 1])
         {
             counter++;
         }
+        // Otherwise, reset counter to 1 for the new number.
         else
         {
             counter = 1;
         }
+
+        // If current number appeared at most twice,
+        // we (keep) it by moving it to the result pointer position.
         if(counter <= 2)
         {
             nums[R] = nums[F];
+            // Then Increments the Rear pointer.
             R++;
         }
+         // Move the Front pointer to continue checking next elements.
         F++;
     }    
+    // The array is now modified in-place; return new valid length.
+    // by Rear pointer who only keep elements that apeeared at most twice. 
     return R;
 }
