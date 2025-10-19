42dc30914353604650ea0bda5c75ff4787e86f5b
Mohamed ElBerry <mohamed_berry210@hotmail.com>
2025-10-19 19:37:59 +0300

    Time: 0 ms (100.00%) | Memory: 22.6 MB (33.28%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -7,4 +7,7 @@ class Solution(object):
         """
         n = len(nums)
         k %= n  # handle cases where k > n
+        # nums[-k:] -> takes the last (k) elements of the array.
+        # nums[:-k] -> take the elements of the array except the last k.
+        # adding them together give us our rotated array by k.
         nums[:] = nums[-k:] + nums[:-k]  # rotate using slicing.
