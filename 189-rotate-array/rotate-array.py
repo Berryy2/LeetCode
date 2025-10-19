f5dfcbb7c0f11bc0110442c7cdcaa65007f73ead
Mohamed ElBerry <mohamed_berry210@hotmail.com>
2025-10-19 19:36:48 +0300

    Time: 0 ms (100.00%) | Memory: 22.7 MB (24.82%) - LeetSync

---
+++ 189-rotate-array/rotate-array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n  # handle cases where k > n
+        nums[:] = nums[-k:] + nums[:-k]  # rotate using slicing.
